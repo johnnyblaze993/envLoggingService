50ef2134a4155f03c5c19e17ef4fd5a0b49c9fb9
Jonathan Alvarez <jalvarez@axientcorp.com>
2024-08-14 21:16:01 +0000

    logs working now. but not trace

--- app/src/main/java/envloggingservice/HelloWorldServlet.java
+++ app/src/main/java/envloggingservice/HelloWorldServlet.java
@@ -1,17 +1,33 @@
 package envloggingservice;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    // Create a logger instance for this class
+    private static final Logger logger = LogManager.getLogger(HelloWorldServlet.class);
+
+    // Constructor to log initialization
+    public HelloWorldServlet() {
+        super();
+        logger.info("HelloWorldServlet initialized and Logger is set up");
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Logging at different levels
+        logger.debug("This is a DEBUG message");
+        logger.trace("This is a TRACE message");
+        logger.info("This is an INFO message");
+        logger.warn("This is a WARN message");
+
+        // Respond with a simple message
         response.setContentType("text/plain");
         response.getWriter().write("Hello, World!");
     }
